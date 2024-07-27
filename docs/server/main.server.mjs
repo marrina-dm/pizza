2760fabaf32a0419a6433a70db3223162a2b4d6d
marrina-dm <marinadmitrieva802@gmail.com>
2024-07-27 22:20:33 +0300

    docs folder

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-METYZABV.mjs";import"./chunk-R6AL6KP3.mjs";import"./chunk-MI45TYF7.mjs";import"./chunk-2RKWHBHQ.mjs";import"./chunk-WR7B2EBC.mjs";import"./chunk-AUYJB7AG.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
